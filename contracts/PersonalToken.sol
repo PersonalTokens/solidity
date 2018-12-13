7db563117a70946ad9e06466fe97ef67a5bd1746
Michał Dutkiewicz <emdeck@gmail.com>
2018-12-13 11:26:19 +0100

    Port PersonalToken contract to OpenZeppelin 2.0

--- contracts/PersonalToken.sol
+++ contracts/PersonalToken.sol
@@ -1,30 +1,21 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.24;
 
-import "./OpenZeppelin/StandardToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 /**
  * @title PersonalToken
  * @dev Personal Token contract.
  */
-contract PersonalToken is StandardToken {
-  string public name = "Token name";
-  string public symbol = "SYMBOL";
-  uint8 public constant decimals = 4;
-
+contract PersonalToken is ERC20, ERC20Detailed {
   /**
    * @dev Mints all tokens to given accounts.
    */
-  function PersonalToken(string _name, string _symbol, address _keeper, address _treasurer) public {
-    name = _name;
-    symbol = _symbol;
-    totalSupply_ = 210000000000;
-
+  constructor(string _name, string _symbol, address _keeper, address _treasurer) public ERC20Detailed(_name, _symbol, 4) {
+    uint256 totalSupply = 210000000000;
     uint256 deposit = 2100000000;
 
-    balances[_keeper] = deposit;
-    balances[_treasurer] = (totalSupply_ - deposit);
-
-    emit Transfer(address(0), _keeper, deposit);
-    emit Transfer(address(0), _treasurer, (totalSupply_ - deposit));
+    _mint(_keeper, deposit);
+    _mint(_treasurer, (totalSupply - deposit));
   }
 }
